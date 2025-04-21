ad49f216233d404f9c1187ab874ab0715156cd6c
Dipal Das <dasdipal8888@gmail.com>
2025-04-21 18:01:34 +0530

    first commit

---
+++ src/test/java/com/dipaldas/usermananagementsystem/UsermananagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipaldas.usermananagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermananagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
